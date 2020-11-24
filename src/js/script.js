69e45fff3eff2ce56e30bcca0d0c16dc7de6eb69
Elijah <elijahmylcev@gmail.com>
2020-11-24 22:17:07 +0300

    normal carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        // dots: true,
         speed: 1200,
-        adaptiveHeight: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-      });
-  });
-  
+        adaptiveHeight: false,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });})
